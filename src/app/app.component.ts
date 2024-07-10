f28120ec95b39984a350cc93a7aacbd4f5b5d38d
Alex <biparreno@sudamericano.edu.ec>
2024-07-10 12:50:30 -0500

    Commit 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,20 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Curso-Programacion';
+  cards = [
+    { id: 1, title: 'Card1', logo: 'assets/image2.jpg', description: 'This is a Bear' },
+    { id: 2, title: 'Card2', logo: 'assets/image.jpg', description: 'This is Bird' },
+    { id: 3, title: 'Card3', logo: 'assets/image3.jpg', description: 'This is a Barracuda' }
+  ];
+
+  showAlert() {
+    alert('¡Bienvenido!');
+  }
+
+  filterById() {
+    this.cards = this.cards.filter(item => item.id > 1);
+  }
 }
